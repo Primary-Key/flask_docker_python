2f5c34c60a474dcbbcc4d9714ba80503950f54db
Piotr Kruczkowski <piotr.przemyslaw.kruczkowski@otpbank.hu>
2022-07-12 09:37:30 +0200

    Working example of python flask docker microservice.

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.9.13-alpine
+WORKDIR /app
+ADD . /app
+RUN pip3 install -r requirements.txt
+CMD ["python3", "app.py"]
---
+++ app.py
@@ -0,0 +1,112 @@
+from flask import Flask, render_template, make_response, jsonify, request
+
+app = Flask(__name__)
+
+PORT = 3200
+HOST = '0.0.0.0'
+
+DATA = {
+    'languages':{
+        'es':'Spanish',
+        'en':'English',
+        'fr':'French',
+    },
+    'colors':{
+        'r':'red',
+        'g':'green',
+        'b':'blue',
+    },
+    'couds':{
+        'IBM':'IBM Cloud',
+        'AMAZON': 'AWS',
+        'MICROSOFT': 'AZURE'
+    }
+}
+
+# GET METHOD
+
+@app.route("/")
+def home():
+    return "<h1 style='color:blue'>This is home.</h1>"
+
+@app.route("/temp")
+def template():
+    return render_template('index.html')
+
+@app.route("/qstr")
+def query_string():
+    if request.args:
+        req = request.args
+        res = {}
+        for key, val in req.items():
+            res[key] = val
+        res = make_response(jsonify(res), 200)
+        return res
+
+    res = make_response({"error":"No query string"}, 400)
+    return res
+
+@app.route("/json")
+def get_json():
+    res = make_response(jsonify(DATA), 200)
+    return res
+
+@app.route("/json/<collection>/<member>")
+def get_data(collection, member):
+    if collection in DATA:
+        member = DATA[collection].get(member)
+        if member:
+            res = make_response(jsonify({"res":member}), 200)
+            return res
+        
+        res = make_response(jsonify({"error":"Member not found"}), 400)
+        return res
+
+    res = make_response(jsonify({"error":"Collection not found"}), 400)
+    return res
+
+# POST METHOD
+
+@app.route("/json/<collection>", methods=["POST"])
+def create_collection(collection):
+    req = request.get_json()
+
+    if collection in DATA:
+        res = make_response(jsonify({"error":"collection already exists"}), 400)
+        return res
+
+    DATA.update({collection: req})
+    res = make_response(jsonify({"res":"collection created"}), 201)
+    return res
+
+# PUT METHOD
+
+@app.route("/json/<collection>/<member>", methods=["PUT"])
+def update_collection(collection, member):
+    req = request.get_json()
+
+    if collection in DATA:
+        if member:
+            DATA[collection][member] = req["new"]
+            res = make_response(jsonify({"res":DATA[collection]}), 200)
+            return res
+
+        res = make_response(jsonify({"error":"Member not specified"}), 400)
+        return res
+    
+    res = make_response(jsonify({"error":"Collection not found"}), 400)
+    return res
+
+@app.route("/json/<collection>", methods=["DELETE"])
+def delete_collection(collection):
+    if collection in DATA:
+        del DATA[collection]
+        res = make_response(jsonify(DATA), 200)
+        return res
+        
+    res = make_response(jsonify({"error":"Collection not found"}), 400)
+    return res
+
+if __name__ == "__main__":
+    print("Server runnning at port %s"%(PORT))
+    app.run(host=HOST, port=PORT)
